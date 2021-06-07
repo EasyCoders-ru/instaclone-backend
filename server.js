e83c6ac24b74fe8df21acaddf89dfda3842a2f07
Anton Golosnichenko <golosnichenko@artics.ru>
2021-06-07 03:07:30 +0300

    3.11 Dotenv

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+require("dotenv").config();
 import { ApolloServer } from "apollo-server";
 import schema from "./schema";
 
 const server = new ApolloServer({ schema });
 
+const PORT = process.env.PORT;
+
 server
-  .listen()
-  .then(() => console.log("Сервер запущен на http://localhost:4000/"));
+  .listen(PORT)
+  .then(() => console.log(`🚀 Сервер запущен на http://localhost:${PORT}/`));
