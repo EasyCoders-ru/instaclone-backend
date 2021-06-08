36fffb30bcd7b1e087dea5aaa64cce8d7efca31f
Anton Golosnichenko <golosnichenko@artics.ru>
2021-06-09 01:14:07 +0300

    4.6 Рефакторинг - часть 2

--- users/createAccount/createAccount.resolvers.js
+++ users/createAccount/createAccount.resolvers.js
@@ -1,6 +1,5 @@
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import client from "../client";
+import client from "../../client";
 
 export default {
   Mutation: {
@@ -27,7 +26,7 @@ export default {
           );
         }
         const hashedPassword = await bcrypt.hash(password, 10);
-        return client.user.create({
+        await client.user.create({
           data: {
             firstName,
             lastName,
@@ -36,34 +35,15 @@ export default {
             password: hashedPassword,
           },
         });
+        return {
+          ok: true,
+        };
       } catch (error) {
-        return error;
-      }
-    },
-    login: async (_, { username, password }) => {
-      const user = await client.user.findFirst({
-        where: {
-          username,
-        },
-      });
-      if (!user) {
         return {
           ok: false,
-          error: "Пользователь не найден",
+          error,
         };
       }
-      const passwordOk = await bcrypt.compare(password, user.password);
-      if (!passwordOk) {
-        return {
-          ok: false,
-          error: "Неправильный пароль",
-        };
-      }
-      const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY);
-      return {
-        ok: true,
-        token,
-      };
     },
   },
 };
