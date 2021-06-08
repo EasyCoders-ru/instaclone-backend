845ea32d65eb782954aa5fbbd7a4c7a4a777f3b1
Anton Golosnichenko <golosnichenko@artics.ru>
2021-06-09 02:27:14 +0300

    4.7 editProfile

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,5 +1,38 @@
+import bcrypt from "bcrypt";
+import client from "../../client";
+
 export default {
   Mutation: {
-    editProfile: () => console.log("Привет!"),
+    editProfile: async (
+      _,
+      { firstName, lastName, username, email, password: newPassword }
+    ) => {
+      let hashedPassword = null;
+      if (newPassword) {
+        hashedPassword = await bcrypt.hash(newPassword, 10);
+      }
+      const editedUser = await client.user.update({
+        where: {
+          id: 1,
+        },
+        data: {
+          firstName,
+          lastName,
+          username,
+          email,
+          ...(hashedPassword && { password: hashedPassword }),
+        },
+      });
+      if (editedUser.id) {
+        return {
+          ok: true,
+        };
+      } else {
+        return {
+          ok: false,
+          error: "Не получилось обновить профиль",
+        };
+      }
+    },
   },
 };
