14c6c4df19bcf52f416bb2421e40765838c78d6f
Anton Golosnichenko <golosnichenko@artics.ru>
2021-06-09 03:53:38 +0300

    4.9 Аутентификация - часть 2

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ require("dotenv").config();
 import { ApolloServer } from "apollo-server";
 import schema from "./schema";
 
-const server = new ApolloServer({ schema });
+const server = new ApolloServer({
+  schema,
+  context: {
+    "token":
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjIzMTk2Nzg4fQ.GV0HUnymNjUdS3i4Z3jlCz5A-zjAYRKuBIqAfbEQ0Hg",
+  },
+});
 
 const PORT = process.env.PORT;
 
