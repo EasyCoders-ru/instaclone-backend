3593566ea0a98d9fca206a1e90dc7fa53fbfaa66
Anton Golosnichenko <golosnichenko@artics.ru>
2021-06-10 00:31:11 +0300

    4.13 Предварительные итоги

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -2,41 +2,41 @@ import bcrypt from "bcrypt";
 import client from "../../client";
 import { protectedResolver } from "../users.utils";
 
+const resolverFn = async (
+  _,
+  { firstName, lastName, username, email, password: newPassword },
+  { loggedInUser }
+) => {
+  let hashedPassword = null;
+  if (newPassword) {
+    hashedPassword = await bcrypt.hash(newPassword, 10);
+  }
+  const editedUser = await client.user.update({
+    where: {
+      id: loggedInUser.id,
+    },
+    data: {
+      firstName,
+      lastName,
+      username,
+      email,
+      ...(hashedPassword && { password: hashedPassword }),
+    },
+  });
+  if (editedUser.id) {
+    return {
+      ok: true,
+    };
+  } else {
+    return {
+      ok: false,
+      error: "Не получилось обновить профиль",
+    };
+  }
+};
+
 export default {
   Mutation: {
-    editProfile: protectedResolver(
-      async (
-        _,
-        { firstName, lastName, username, email, password: newPassword },
-        { loggedInUser }
-      ) => {
-        let hashedPassword = null;
-        if (newPassword) {
-          hashedPassword = await bcrypt.hash(newPassword, 10);
-        }
-        const editedUser = await client.user.update({
-          where: {
-            id: loggedInUser.id,
-          },
-          data: {
-            firstName,
-            lastName,
-            username,
-            email,
-            ...(hashedPassword && { password: hashedPassword }),
-          },
-        });
-        if (editedUser.id) {
-          return {
-            ok: true,
-          };
-        } else {
-          return {
-            ok: false,
-            error: "Не получилось обновить профиль",
-          };
-        }
-      }
-    ),
+    editProfile: protectedResolver(resolverFn),
   },
 };
