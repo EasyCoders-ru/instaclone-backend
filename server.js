0dc033f20b46a95ed2251e09aa054d02754620af
Anton Golosnichenko <golosnichenko@artics.ru>
2021-06-11 14:06:04 +0300

    4.18 Установка Express

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 require("dotenv").config();
+import express from "express";
+import logger from "morgan";
 import { ApolloServer } from "apollo-server-express";
 import { typeDefs, resolvers } from "./schema";
 import { getUser, protectResolvers } from "./users/users.utils";
@@ -16,6 +18,11 @@ const server = new ApolloServer({
 
 const PORT = process.env.PORT;
 
-server
-  .listen(PORT)
-  .then(() => console.log(`🚀 Сервер запущен на http://localhost:${PORT}/`));
+const app = express();
+app.use(logger("tiny"));
+
+server.applyMiddleware({ app });
+
+app.listen({ port: PORT }, () => {
+  console.log(`🚀 Сервер запущен на http://localhost:${PORT}/`);
+});
