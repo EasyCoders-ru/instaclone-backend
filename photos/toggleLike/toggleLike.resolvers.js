6c1f6135b700f711ff7571a133de223be9b53fa3
Anton Golosnichenko <golosnichenko@artics.ru>
2021-06-14 19:04:42 +0300

    6.10 Создание и удаление лайка - часть 2

--- photos/toggleLike/toggleLike.resolvers.js
+++ photos/toggleLike/toggleLike.resolvers.js
@@ -2,7 +2,7 @@ import client from "../../client";
 import { protectedResolver } from "../../users/users.utils";
 
 export default {
-  Muatation: {
+  Mutation: {
     toggleLike: protectedResolver(async (_, { id }, { loggedInUser }) => {
       const ok = await client.photo.findUnique({ where: { id } });
       if (!ok) {
@@ -11,6 +11,30 @@ export default {
           error: "Фотография не найдена",
         };
       }
+      const like_where = {
+        photoId_userId: {
+          photoId: id,
+          userId: loggedInUser.id,
+        },
+      };
+      const like = await client.like.findUnique({
+        where: like_where,
+      });
+      if (like) {
+        await client.like.delete({
+          where: like_where,
+        });
+      } else {
+        await client.like.create({
+          data: {
+            user: { connect: { id: loggedInUser.id } },
+            photo: { connect: { id } },
+          },
+        });
+      }
+      return {
+        ok: true,
+      };
     }),
   },
 };
