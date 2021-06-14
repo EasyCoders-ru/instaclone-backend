a124b5ea067da6a71eea6719b32b4b4bdd3f78a6
Anton Golosnichenko <golosnichenko@artics.ru>
2021-06-14 19:35:10 +0300

    6.11 Просмотр лайков фото

---
+++ photos/seePhotoLikes/seePhotoLikes.resolvers.js
@@ -0,0 +1,23 @@
+import client from "../../client";
+
+export default {
+  Query: {
+    seePhotoLikes: async (_, { id }) => {
+      const likes = await client.like.findMany({
+        where: {
+          photoId: id,
+        },
+        select: {
+          user: {
+            select: {
+              username: true,
+              bio: true,
+              avatar: true,
+            },
+          },
+        },
+      });
+      return likes.map((like) => like.user);
+    },
+  },
+};
