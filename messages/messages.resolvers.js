3c24ec2fc7055f27a7584de5c78506d14d12405a
Anton Golosnichenko <golosnichenko@artics.ru>
2021-06-18 03:06:57 +0300

    7.6 Тестирование запросов

--- messages/messages.resolvers.js
+++ messages/messages.resolvers.js
@@ -3,18 +3,21 @@ import client from "../client";
 export default {
   Dialog: {
     users: ({ id }) => client.dialog.findUnique({ where: { id } }).users(),
-    messages: ({ id }) => client.messages.findMany({ where: { dialogId: id } }),
+    messages: ({ id }) => client.message.findMany({ where: { dialogId: id } }),
     unreadTotal: ({ id }, _, { loggedInUser }) => {
       if (!loggedInUser) {
         return 0;
       }
-      return client.messages.count({
+      return client.message.count({
         where: {
           read: false,
           dialogId: id,
-          users: { id: { not: loggedInUser.id } },
+          user: { id: { not: loggedInUser.id } },
         },
       });
     },
+  },
+  Message: {
+    user: ({ id }) => client.message.findUnique({ where: { id } }).user(),
   },
 };
