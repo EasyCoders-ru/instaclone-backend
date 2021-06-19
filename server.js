1c384ec4c7262b5ef8e9a03a2ffe22590565d29a
Anton Golosnichenko <golosnichenko@artics.ru>
2021-06-19 20:35:34 +0300

    7.8 Настройка подписки - часть 2

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 import express from "express";
 import logger from "morgan";
+import http from "http";
 import { ApolloServer } from "apollo-server-express";
 import { typeDefs, resolvers } from "./schema";
 import { getUser, protectedResolver } from "./users/users.utils";
@@ -9,10 +10,12 @@ const apollo = new ApolloServer({
   resolvers,
   typeDefs,
   context: async ({ req }) => {
-    return {
-      loggedInUser: await getUser(req.headers.token),
-      protectedResolver,
-    };
+    if (req) {
+      return {
+        loggedInUser: await getUser(req.headers.token),
+        protectedResolver,
+      };
+    }
   },
 });
 
@@ -21,9 +24,11 @@ const PORT = process.env.PORT;
 const app = express();
 app.use(logger("tiny"));
 apollo.applyMiddleware({ app });
-apollo.installSubscriptionHandlers(app);
 app.use("/static", express.static("uploads"));
 
-app.listen({ port: PORT }, () => {
+const httpServer = http.createServer(app);
+apollo.installSubscriptionHandlers(httpServer);
+
+httpServer.listen(PORT, () => {
   console.log(`🚀 Сервер запущен на http://localhost:${PORT}/`);
 });
