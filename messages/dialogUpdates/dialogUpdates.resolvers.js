71f1456b2f26489c9382cac3cdaf2e10cec72940
Anton Golosnichenko <golosnichenko@artics.ru>
2021-06-19 22:11:12 +0300

    7.10 Фильтрация подписок - часть 2

--- messages/dialogUpdates/dialogUpdates.resolvers.js
+++ messages/dialogUpdates/dialogUpdates.resolvers.js
@@ -1,16 +1,26 @@
 import { withFilter } from "apollo-server-express";
+import client from "../../client";
 import { NEW_MESSAGE } from "../../constants";
 import pubsub from "../../pubsub";
 
 export default {
   Subscription: {
     dialogUpdates: {
-      subscribe: withFilter(
-        () => pubsub.asyncIterator(NEW_MESSAGE),
-        ({ dialogUpdates }, { id }) => {
-          return dialogUpdates.dialogId === id;
+      subscribe: async (root, args, context, info) => {
+        const dialog = client.dialog.findUnique({
+          where: { id: args.id },
+          select: { id: true },
+        });
+        if (!dialog) {
+          throw new Error("Нет доступа к диалогу");
         }
-      ),
+        return withFilter(
+          () => pubsub.asyncIterator(NEW_MESSAGE),
+          ({ dialogUpdates }, { id }) => {
+            return dialogUpdates.dialogId === id;
+          }
+        )(root, args, context, info);
+      },
     },
   },
 };
