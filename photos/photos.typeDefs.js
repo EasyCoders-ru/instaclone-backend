6fa309d115adabea849f6e5a89b20f53fd69aef2
Anton Golosnichenko <golosnichenko@artics.ru>
2021-08-07 09:30:26 +0300

    11.11 Комментарии - часть 1

--- photos/photos.typeDefs.js
+++ photos/photos.typeDefs.js
@@ -7,7 +7,8 @@ export default gql`
     file: String!
     caption: String
     likes: Int!
-    comments: Int!
+    commentsNumber: Int!
+    comments: [Comment]
     hashtags: [Hashtag]
     createdAt: String!
     updatedAt: String!
